fd4eff02385d2f158b2d1d611782329f6a7fd9ae
A Nazarov <roman_emperor5@mail.ru>
2020-11-25 21:39:35 +0500

    uploaded font awesome react icons to our project

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Player = () => {
   return (
     <div className="player">
@@ -6,7 +13,11 @@ const Player = () => {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+      </div>
     </div>
   );
 };
